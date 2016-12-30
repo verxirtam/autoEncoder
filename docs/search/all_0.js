1b4c06e330dad4d848a6db20220d4a2ec4a4646c
verxirtam <d-kosuga@jcom.home.ne.jp>
2016-12-31 00:58:36 +0900

    update docs.

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -8,5 +8,6 @@ var searchData=
   ['backpropagation_5fcuda_2ecu',['Backpropagation_cuda.cu',['../Backpropagation__cuda_8cu.html',1,'']]],
   ['backpropagationalltest',['BackpropagationAllTest',['../classBackpropagationAllTest.html',1,'']]],
   ['backpropagationtest',['BackpropagationTest',['../classBackpropagationTest.html',1,'']]],
+  ['backpropagationtest_5fall_5fshowinfo',['BackpropagationTest_All_showInfo',['../unittest_8cpp.html#aae55169dd6fed4ee427f3a8ec411f8f0',1,'unittest.cpp']]],
   ['bias',['bias',['../classBackpropagation.html#a58d2ed6e8228e965247f2ebeb76643ca',1,'Backpropagation']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['back',['back',['../classBackpropagation.html#a7ff957d5f4bc7f10bc9e84d317296b4e',1,'Backpropagation']]],
-  ['backpropagation',['Backpropagation',['../classBackpropagation.html#a6b0f4dc317a9ae9e58416835757752d5',1,'Backpropagation']]]
+  ['backpropagation',['Backpropagation',['../classBackpropagation.html#a6b0f4dc317a9ae9e58416835757752d5',1,'Backpropagation']]],
+  ['backpropagationtest_5fall_5fshowinfo',['BackpropagationTest_All_showInfo',['../unittest_8cpp.html#aae55169dd6fed4ee427f3a8ec411f8f0',1,'unittest.cpp']]]
 ];
