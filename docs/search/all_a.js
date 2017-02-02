43bae08de83d497742305facd91d0032af0466d9
verxirtam <d-kosuga@jcom.home.ne.jp>
2017-02-03 01:57:40 +0900

    update docs.

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -4,5 +4,6 @@ var searchData=
   ['normalization_2ecpp',['Normalization.cpp',['../Normalization_8cpp.html',1,'']]],
   ['normalization_2eh',['Normalization.h',['../Normalization_8h.html',1,'']]],
   ['normalization_5fcuda_2ecu',['Normalization_cuda.cu',['../Normalization__cuda_8cu.html',1,'']]],
+  ['normalizationgeneraltest',['NormalizationGeneralTest',['../classNormalizationGeneralTest.html',1,'']]],
   ['normalizationtest',['NormalizationTest',['../classNormalizationTest.html',1,'']]]
 ];
