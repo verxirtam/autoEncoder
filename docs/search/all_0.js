8928d36a25da952b5275c392b497150cbe617a66
verxirtam <d-kosuga@jcom.home.ne.jp>
2017-10-07 18:01:38 +0900

    update docs.

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f1b',['_1B',['../classBackpropagation.html#ab21e885b9b95867d054d4d5117d33c65',1,'Backpropagation']]]
+  ['_5f1b',['_1B',['../classAutoEncoder.html#a3fd5d2c87db534b89d2c3263fc328a71',1,'AutoEncoder::_1B()'],['../classBackpropagation.html#ab21e885b9b95867d054d4d5117d33c65',1,'Backpropagation::_1B()']]]
 ];
