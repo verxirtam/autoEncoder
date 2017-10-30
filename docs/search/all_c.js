367e85a8677ee2464fc9ad15daaad9e7959ca388
verxirtam <d-kosuga@jcom.home.ne.jp>
2017-10-31 02:39:47 +0900

    update docs.

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['normalization',['Normalization',['../classNormalization.html',1,'Normalization'],['../classNormalization.html#a7998284e5456d915ebdb2520e4d74250',1,'Normalization::Normalization()'],['../classAutoEncoder.html#a887befd953ae46e976fbf2706ef77fa1',1,'AutoEncoder::normalization()']]],
+  ['normalization',['Normalization',['../classNormalization.html',1,'Normalization'],['../classAutoEncoder.html#a887befd953ae46e976fbf2706ef77fa1',1,'AutoEncoder::normalization()'],['../classNormalization.html#a7998284e5456d915ebdb2520e4d74250',1,'Normalization::Normalization()']]],
   ['normalization_2ecpp',['Normalization.cpp',['../Normalization_8cpp.html',1,'']]],
   ['normalization_2eh',['Normalization.h',['../Normalization_8h.html',1,'']]],
   ['normalization_5fcuda_2ecu',['Normalization_cuda.cu',['../Normalization__cuda_8cu.html',1,'']]],
