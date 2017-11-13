a5c0b405a864cc7151483abd02ab2c1635f2a2c8
verxirtam <d-kosuga@jcom.home.ne.jp>
2017-11-14 03:19:00 +0900

    update docs.

--- docs/search/classes_5.js
+++ docs/search/classes_5.js
@@ -7,7 +7,9 @@ var searchData=
   ['func1to1tanh',['Func1to1Tanh',['../classFunc1to1Tanh.html',1,'']]],
   ['func2to1composite1st',['Func2to1Composite1st',['../classFunc2to1Composite1st.html',1,'']]],
   ['func2to1composite2nd',['Func2to1Composite2nd',['../classFunc2to1Composite2nd.html',1,'']]],
+  ['func2to1elementwisedifference',['Func2to1ElementWiseDifference',['../classFunc2to1ElementWiseDifference.html',1,'']]],
   ['func2to1elementwiseproduct',['Func2to1ElementWiseProduct',['../classFunc2to1ElementWiseProduct.html',1,'']]],
+  ['func2to1elementwisesum',['Func2to1ElementWiseSum',['../classFunc2to1ElementWiseSum.html',1,'']]],
   ['fxautoencoder',['FXAutoEncoder',['../classFXAutoEncoder.html',1,'']]],
   ['fxautoencodertest',['FXAutoEncoderTest',['../classFXAutoEncoderTest.html',1,'']]]
 ];
